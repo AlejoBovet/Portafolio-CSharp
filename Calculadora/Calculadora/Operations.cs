79ddec48e16934910c00aa8df0d086f7bf851c14
Alejandro Bovet <102919972+AlejoBovet@users.noreply.github.com>
2025-06-24 16:43:34 -0400

    aplicacion Calculadora

---
+++ Calculadora/Calculadora/Operations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Operations
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+
+        public double Sumar(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+            return Num1 + Num2;
+        }
+
+        public double Resta(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+            return Num1 - Num2;
+        }
+
+        public double Multiplicacion(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+            return Num1 * Num2;
+        }
+
+        public double Dividir(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+
+            if (Num2 == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir por cero.");
+            }
+
+            return Num1 / Num2;
+        }
+
+
+    }
+
+}
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a calculadora");
+
+            Console.WriteLine("Ingrese una opcion por favor");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Ha seleccionado Suma");
+
+                    Console.WriteLine("Ingrese el primer numero");
+                    double nums1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero");
+                    double nums2 = double.Parse(Console.ReadLine());
+
+                    Operations operationSum = new Operations();
+                    double resultadoSum = operationSum.Sumar(nums1, nums2);  
+
+                    Console.WriteLine($"Resultado: {resultadoSum}"); 
+                    break;
+
+                case 2:
+                    Console.WriteLine("Ha seleccionado Resta");
+               
+                    Console.WriteLine("Ingrese el primer numero");
+                    double numr1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero");
+                    double numr2 = double.Parse(Console.ReadLine());
+
+                    Operations operationRest = new Operations();
+                    double resultadoRest = operationRest.Resta(numr1, numr2);
+
+                    Console.WriteLine($"Resultado: {resultadoRest}");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Multiplicacion");
+                    Console.WriteLine("Ha seleccionado Suma");
+
+                    Console.WriteLine("Ingrese el primer numero");
+                    double numm1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero");
+                    double numm2 = double.Parse(Console.ReadLine());
+
+                    Operations operationMult = new Operations();
+                    double resultadoMult = operationMult.Multiplicacion(numm1, numm2);
+
+                    Console.WriteLine($"Resultado: {resultadoMult}");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Divicion");
+                    Console.WriteLine("Ha seleccionado Suma");
+
+                    Console.WriteLine("Ingrese el primer numero");
+                    double numd1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese el segundo numero");
+                    double numd2 = double.Parse(Console.ReadLine());
+
+                    Operations operationDiv = new Operations();
+                    double resultadoDiv = operationDiv.Dividir(numd1, numd2);
+
+                    Console.WriteLine($"Resultado: {resultadoDiv}");
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
