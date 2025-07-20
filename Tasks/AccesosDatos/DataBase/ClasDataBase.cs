e14d354e714b674b3b247ca47617514a24d2f68a
Alejandro Bovet <102919972+AlejoBovet@users.noreply.github.com>
2025-07-20 11:57:32 -0400

    Creata proyect Tasks
    - Se crea base de proyecto de tareas

---
+++ Tasks/AccesosDatos/DataBase/ClasDataBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient; 
+
+namespace AccesosDatos.DataBase
+{
+    internal class ClasDataBase
+    {
+            private string connectionString;
+            private SqlConnection connection;
+
+            public ClasDataBase(string connectionString)
+            {
+                this.connectionString = connectionString;
+                this.connection = new SqlConnection(connectionString);
+            }
+
+            // Abrir conexión
+            public void Open()
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexión abierta.");
+                }
+            }
+
+            // Cerrar conexión
+            public void Close()
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                    Console.WriteLine("Conexión cerrada.");
+                }
+            }
+
+            // Ejecutar una consulta SELECT de ejemplo
+            public void ExecuteSelect(string query)
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Primera columna: {reader[0]}");
+                    }
+                }
+            }
+
+            // Devuelve la conexión por si quieres usarla desde afuera
+            public SqlConnection GetConnection()
+            {
+                return connection;
+            }
+        }
+}
---
+++ Tasks/LogicaNegocio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicaNegocio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
